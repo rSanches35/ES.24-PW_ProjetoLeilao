86d1f9144bb50fd55b3c91c5b5e8e4819dd4c665
rXanxess <rafaelsanches.s35@gmail.com>
2024-08-17 17:13:10 -0300

    Routes da Página Register

---
+++ frontend/src/pages/register/Register.jsx
@@ -0,0 +1,13 @@
+import React from "react";
+import { Helmet } from 'react-helmet';
+
+import './Register.css'
+
+const Register = () => {
+
+    return (
+        <div>
+            <Helmet><title>Register</title></Helmet>
+        </div>
+    );
+}; export default Register;
