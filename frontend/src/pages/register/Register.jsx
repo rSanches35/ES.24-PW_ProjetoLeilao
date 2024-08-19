77f75c99e5fe16d465ef5c05acfb41019ca84e93
rXanxess <rafaelsanches.s35@gmail.com>
2024-08-19 14:35:55 -0300

    Requisições da Senha

--- frontend/src/pages/register/Register.jsx
+++ frontend/src/pages/register/Register.jsx
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
 import { Helmet } from 'react-helmet';
 import { Link } from 'react-router-dom';
 
@@ -6,6 +6,7 @@ import './Register.css'
 
 import { Card } from 'primereact/card';
 import { Button } from 'primereact/button';
+import { Divider } from "primereact/divider";
 import { Password } from 'primereact/password';
 import { Calendar } from 'primereact/calendar';
 import { InputText } from 'primereact/inputtext';
@@ -13,6 +14,70 @@ import { InputMask } from 'primereact/inputmask';
         
 
 const Register = () => {
+
+    const [password, setPassword] = useState("");
+    const [passwordConfirm, setPasswordConfirm] = useState("");
+    const [errorMessage, setErrorMessage] = useState("");
+    const [passwordRequirements, setPasswordRequirements] = useState({
+        minLength: false,
+        hasLowerCase: false,
+        hasUpperCase: false,
+        hasNumber: false,
+        hasSpecialChar: false,
+    });
+  
+    const header = <div className="font-bold mb-3">Choose Password</div>;
+    const footer = (
+        <>
+        <Divider />
+        <p className="mt-2">Requirements</p>
+        <ul className="pl-2 ml-2 mt-0 line-height-3">
+            <li
+                className={passwordRequirements.minLength ? "text-green-500" : "text-red-500"}>
+                Minimum characters [6]
+            </li>
+            <li
+                className={passwordRequirements.hasLowerCase ? "text-green-500" : "text-red-500"}>
+                Lowercase
+            </li>
+            <li
+                className={passwordRequirements.hasUpperCase ? "text-green-500" : "text-red-500"}>
+                Uppercase
+            </li>
+            <li
+                className={passwordRequirements.hasNumber ? "text-green-500" : "text-red-500"}>
+                Numeric
+            </li>
+            <li
+                className={passwordRequirements.hasSpecialChar ? "text-green-500" : "text-red-500"}>
+                Special character
+            </li>
+        </ul>
+        </>
+    );
+  
+    const onPasswordChange = (e) => {
+
+        const typedPassword = e.target.value;
+
+        setPassword(typedPassword);
+        setPasswordRequirements({
+            minLength: typedPassword.length >= 6,
+            hasLowerCase: /[a-z]/.test(typedPassword),
+            hasUpperCase: /[A-Z]/.test(typedPassword),
+            hasNumber: /[0-9]/.test(typedPassword),
+            hasSpecialChar: /[!@#$%^&*(),.?":{}|<>]/.test(typedPassword),
+        });
+    };
+  
+    const onPasswordConfirmChange = (e) => {
+
+        const passwordConfirm = e.target.value;
+
+        setPasswordConfirm(passwordConfirm);
+        if (password !== passwordConfirm) { setErrorMessage("Passwords are different");}
+        else { setErrorMessage("");}
+    };
 
     return (
         <div>
@@ -46,9 +111,13 @@ const Register = () => {
                 <Password
                 placeholder="Password"
                 toggleMask
-                feedback={false}
                 className="mt-3 w-10"
                 inputStyle={{ width: '100%' }}
+
+                header={header}
+                footer={footer}
+                value={password}
+                onChange={onPasswordChange}
                 />
 
                 <Password
@@ -57,7 +126,12 @@ const Register = () => {
                 feedback={false}
                 className="mt-3 w-10"
                 inputStyle={{ width: '100%' }}
+
+                value={passwordConfirm}
+                onChange={onPasswordConfirmChange}
                 />
+
+                {errorMessage && <p className="text-red-500">{errorMessage}</p>}
 
                 <Button
                 label="Register"
