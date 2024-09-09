7f8ee0e04cacb6ed8a83e9d5db3b0bf2d2e8ea89
rSanches35 <rafaelsanches.s35@gmail.com>
2024-09-09 14:19:44 -0300

    API Automatização de CEP

--- frontend/src/pages/profile/Profile.jsx
+++ frontend/src/pages/profile/Profile.jsx
@@ -8,13 +8,19 @@ import { Avatar } from 'primereact/avatar';
 import { Button } from 'primereact/button';
 import { Dialog } from 'primereact/dialog';
 import { InputText } from 'primereact/inputtext';
+import { InputMask } from 'primereact/inputmask';
+import { InputTextarea } from 'primereact/inputtextarea';
+
+import axios from 'axios';
 
 const Profile = () => {
 
     const [showDialog, setShowDialog] = useState(false); // State to control dialog visibility
     const [formData, setFormData] = useState({
         state: '',
+        stateCode: '',
         city: '',
+        zipCode: null,
         street: '',
         number: '',
         neighborhood: '',
@@ -24,10 +30,31 @@ const Profile = () => {
     const handleInputChange = (e) => {
         const { name, value } = e.target;
         setFormData({ ...formData, [name]: value });
+        
+        if (name === "zipCode" && value.length === 9) {
+            fetchAddressFromZipCode(value);
+        }
+    };
+
+    const fetchAddressFromZipCode = async (zipCode) => {
+        try {
+            const response = await axios.get(`https://viacep.com.br/ws/${zipCode}/json/`);
+            const { estado, uf, localidade, logradouro, bairro } = response.data;
+            
+            setFormData((prevData) => ({
+                ...prevData,
+                state: estado || prevData.state,
+                stateCode: uf || prevData.state,
+                city: localidade || prevData.city,
+                street: logradouro || prevData.street,
+                neighborhood: bairro || prevData.neighborhood
+            }));
+        } catch (error) {
+            console.error("Error fetching address from ViaCEP:", error);
+        }
     };
 
     const handleSubmit = () => {
-        
         console.log(formData);
         setShowDialog(false);
     };
@@ -84,13 +111,13 @@ const Profile = () => {
                         </div>
                         <div className="mini-card in-progress">
                             <h3>
-                            <i className="pi pi-spin pi-cog ml-1 mr-2" style={{ fontSize: '1.2rem' }}></i>
+                                <i className="pi pi-spin pi-cog ml-1 mr-2" style={{ fontSize: '1.2rem' }}></i>
                                 In Progress...
                             </h3>
                         </div>
                         <div className="mini-card in-progress">
                             <h3>
-                            <i className="pi pi-spin pi-cog ml-1 mr-2" style={{ fontSize: '1.2rem' }}></i>
+                                <i className="pi pi-spin pi-cog ml-1 mr-2" style={{ fontSize: '1.2rem' }}></i>
                                 In Progress...
                             </h3>
                         </div>
@@ -105,45 +132,103 @@ const Profile = () => {
             </Card>
 
             <Dialog header="Edit Address" visible={showDialog} style={{ width: '400px' }} modal onHide={() => setShowDialog(false)}>
-                <div className="p-fluid">
-                    <div className="p-field">
-                        <label htmlFor="state">State</label>
-                        <InputText id="state" name="state" value={formData.state} onChange={handleInputChange} />
+                <div className="p-fluid edit-dialog-fields pt-1">
+
+                    <div className="p-field flex justify-content-between row mb-3">
+                        <div className="p-field col-9 p-0 pr-3">
+                            <InputText
+                            placeholder="State"
+                            id="state"
+                            name="state"
+                            value={formData.state}
+                            onChange={handleInputChange}
+                            />
+                        </div>
+                        <div className="p-field col-3 p-0 ">
+                        <InputText 
+                        placeholder="Code"
+                        id="stateCode"
+                        name="stateCode"
+                        value={formData.stateCode}
+                        onChange={(e) => { const value = e.target.value.toUpperCase();
+                            if (/^[A-Za-z]*$/.test(value) && value.length <= 2) {
+                                setFormData({ ...formData, stateCode: value });}
+                        }}
+                        Length={2}
+                        />
+                        </div>
                     </div>
 
-                    <div className="p-field">
-                        <label htmlFor="city">City</label>
-                        <InputText id="city" name="city" value={formData.city} onChange={handleInputChange} />
+                    <div className="p-field mb-3">
+                        <InputText
+                        placeholder="City"
+                        id="city"
+                        name="city"
+                        value={formData.city}
+                        onChange={handleInputChange}
+                        />
                     </div>
 
-                    <div className="p-field">
-                        <label htmlFor="street">ZIP Code</label>
-                        <InputText id="street" name="street" value={formData.street} onChange={handleInputChange} />
+                    <div className="p-field mb-3">
+                        <InputMask
+                        placeholder="ZIP Code"
+                        mask="99999-999"
+                        id="zipCode"
+                        name="zipCode"
+                        value={formData.zipCode}
+                        onChange={handleInputChange}
+                        />
                     </div>
 
-                    <div className="p-field">
-                        <label htmlFor="street">Street</label>
-                        <InputText id="street" name="street" value={formData.street} onChange={handleInputChange} />
+                    <div className="p-field mb-3">
+                        <InputText
+                        placeholder="Street"
+                        id="street"
+                        name="street"
+                        value={formData.street}
+                        onChange={handleInputChange}
+                        />
                     </div>
 
-                    <div className="p-field">
-                        <label htmlFor="number">Number</label>
-                        <InputText id="number" name="number" value={formData.number} onChange={handleInputChange} />
+                    <div className="p-field flex justify-content-between row mb-3">
+                        <div className="p-field col-9 p-0 pr-3">
+                            <InputText
+                            placeholder="Neighborhood"
+                            id="neighborhood"
+                            name="neighborhood"
+                            value={formData.neighborhood}
+                            onChange={handleInputChange}
+                            />
+                        </div>
+                        <div className="p-field col-3 p-0">
+                            <InputText
+                            placeholder="Number"
+                            id="number"
+                            name="number"
+                            value={formData.number}
+                            onChange={handleInputChange}
+                            />
+                        </div>
                     </div>
 
-                    <div className="p-field">
-                        <label htmlFor="neighborhood">Neighborhood</label>
-                        <InputText id="neighborhood" name="neighborhood" value={formData.neighborhood} onChange={handleInputChange} />
+                    <div className="p-field mb-4">
+                        <InputTextarea
+                        rows={5}
+                        cols={30}
+                        autoResize
+                        placeholder="Notes"
+                        id="additionalInfo"
+                        name="additionalInfo"
+                        value={formData.additionalInfo}
+                        onChange={handleInputChange}
+                        />
                     </div>
 
-                    <div className="p-field">
-                        <label htmlFor="additionalInfo">Additional Info (Optional)</label>
-                        <InputText id="additionalInfo" name="additionalInfo" value={formData.additionalInfo} onChange={handleInputChange} />
-                    </div>
-
-                    <Button label="Save" onClick={handleSubmit} />
+                    <Button label="Save" onClick={handleSubmit}/>
                 </div>
             </Dialog>
         </div>
     );
-}; export default Profile;
+};
+
+export default Profile;
