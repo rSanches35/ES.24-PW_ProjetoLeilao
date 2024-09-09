4ffe1a398d305cb277bad29db502f3403373bd10
rSanches35 <rafaelsanches.s35@gmail.com>
2024-09-09 19:59:08 -0300

    Pequenos Ajustes de i18n

--- frontend/src/pages/dashboard/Dashboard.jsx
+++ frontend/src/pages/dashboard/Dashboard.jsx
@@ -1,13 +1,16 @@
 import React from "react";
 import { Helmet } from 'react-helmet';
+import { useTranslation } from "react-i18next";
 
 import './Dashboard.css'
 
 const Dashboard = () => {
 
+    const { t } = useTranslation();
+
     return (
-        <div className="body-home">
-            <Helmet><title>Dashboard - Maxin'UP</title></Helmet>
+        <div className="body-dashboard">
+            <Helmet><title>{t('dashboard')} - Maxin'UP</title></Helmet>
         </div>
     );
 }; export default Dashboard;
--- frontend/src/pages/home/Home.jsx
+++ frontend/src/pages/home/Home.jsx
@@ -1,13 +1,16 @@
 import React from "react";
 import { Helmet } from 'react-helmet';
+import { useTranslation } from "react-i18next";
 
 import './Home.css'
 
 const Home = () =>{
 
+    const { t } = useTranslation();
+
     return(
         <div className="body-home">
-            <Helmet><title>Home - Maxin'UP</title></Helmet>
+            <Helmet><title>{t('home')} - Maxin'UP</title></Helmet>
         </div>
     );
 }; export default Home;
